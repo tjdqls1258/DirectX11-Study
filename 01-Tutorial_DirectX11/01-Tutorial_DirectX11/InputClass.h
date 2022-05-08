b39ec59627e075fd95ce7ed29d3dc5ab41786aa5
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2022-05-08 15:35:31 +0900

    05-08 준비 단계
     준비 단계

---
+++ 01-Tutorial_DirectX11/01-Tutorial_DirectX11/InputClass.h
@@ -0,0 +1,20 @@
+#ifndef _INPUTCLASS_H_
+#define _INPUTCLASS_H_
+
+class InputClass
+{
+private:
+	bool m_keys[256];
+public: 
+	InputClass(); 
+	InputClass(const InputClass&); 
+	~InputClass(); 
+
+	void Initialize();
+
+	void KeyDown(unsigned int);
+	void KeyUp(unsigned int);
+	bool IsKeyDown(unsigned int);
+};
+
+#endif // !_INPUTCLASS_H_
